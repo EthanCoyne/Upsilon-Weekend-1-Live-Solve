e4a634bb07314443746ea8847b7f1b2994dbccab
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-09 10:46:31 -0600

    new rows being added

--- client.js
+++ client.js
@@ -1,22 +1,30 @@
 $(function(){
   console.log('jQuery is loaded correctly!!!');
   $('#newEmployeeForm').on('submit', function(event){
-    event.preventDefault();
+    event.preventDefault(); // stop page from reloading and redirecting
     console.log('Form has been submitted!!');
 
-    var newEmployeeArray = $(this).serializeArray();
+    var newEmployeeArray = $(this).serializeArray(); // get the information from the form into an array
     // console.log(newEmployee);
 
     var newEmployeeObject = {};
 
+// loop through all the input properties in the array to make a single object
     for(var i =0; i < newEmployeeArray.length; i++){
-      // console.log(newEmployeeArray[i]);
       var inputFieldName = newEmployeeArray[i].name;
       var inputFieldValue = newEmployeeArray[i].value;
-      // console.log('The name of the value ',  i, ' is ', inputFieldName);
-      // console.log('The name of the value ',  i, ' is ', inputFieldValue);
       newEmployeeObject[inputFieldName] = inputFieldValue;
     }
     console.log(newEmployeeObject);
+
+  var newRow =   '<tr>' +
+      '<td>' + newEmployeeObject.firstName + '</td>' +
+      '<td>' + newEmployeeObject.lastName + '</td>' +
+      '<td>' + newEmployeeObject.number + '</td>' +
+      '<td>' + newEmployeeObject.title + '</td>' +
+      '<td>' + newEmployeeObject.salary + '</td>' +
+    '</tr>';
+
+    $('#employeesTable').append(newRow)
   });
 });
